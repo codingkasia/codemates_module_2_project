d0dc781d3ef5b70977281c4dcf4254dcf52ee043
codingkasia <codingkasia@gmail.com>
2018-06-01 09:45:03 -0400

    before cohort message board

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,5 +1,6 @@
 class Cohort < ApplicationRecord
     belongs_to :location
     has_many :users
-    has_many :comments, through: :users 
+    has_many :comments, through: :users, foreign_key: 'receiver_id'
+   
 end
